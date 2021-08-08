dbe867a7acd778bf369a031c421949cbf854eae0
soulfly34 <75480587+soulfly34@users.noreply.github.com>
2021-08-08 20:20:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,49 @@
-// Bot Soulfly#9062 Tarafınca Yapılmıştır, Satış Yapılması Yasaktır.
-// Bot Denenmiş Ve Tamamen Çalışmaktadır.
-
 const Discord = require('discord.js'), 
       client = new Discord.Client({ ws: { intents: Discord.Intents.ALL }}), 
       ytdlDiscord = require("discord-ytdl-core");
 
 client.on('ready', async() => {
   client.user.setStatus("dnd");
-  client.user.setActivity(" Ses botu yaptırmak için: Soulfly#9062 ")
-  play()  
+  client.user.setActivity("Voice Bot & Soulfly#1111");
+  play(); // Begin.
 })
 
 
-const setting = {
-  channelID: "", // Ses Kanalının ID Giriniz.
-  modID: "", // Moderatör RolID Giriniz.
-  videoURL: "" // Youtube Video Link.
+const { channelID, modID, videoURL, clientToken } = {
+  clientToken: "", // You'r Application Token
+  channelID: "", // Voice Channel ID
+  modID: "", // Staff Role ID
+  videoURL: "" // Youtube Music & Video URL
 }
 
-client.on('voiceStateUpdate', async function(oldState, newState){
+client.on('voiceStateUpdate', (oldState, newState) => {
   if((oldState.member && oldState.member.user.bot) || (newState.member && newState.member.user.bot)) return;
-  if(newState.channelID == setting.channelID){
-    if(client.channels.cache.get(setting.channelID).members.some(member => member.roles.cache.has(setting.modID)) == true) client.channels.cache.get(setting.channelID).leave()
-  } else if(oldState.channelID == setting.channelID){
-    if(client.channels.cache.get(setting.channelID).members.some(member => member.roles.cache.has(setting.modID)) == false)  play()
+
+  let isMod = client.channels.cache.get(channelID).members.some(member => member.roles.cache.has(modID));
+  if(newState.channelID == channelID && isMod){
+    client.channels.cache.get(channelID).leave();
+  } else if(oldState.channelID == channelID && !isMod){
+    play();
   }
 })
 
 
-async function play(){ 
-  let url = await ytdlDiscord(setting.videoURL, {
-            filter: "audioonly",
-            opusEncoded: true,
-            encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
-    });
-  let streamType = setting.videoURL.includes("youtube.com") ? "opus" : "ogg/opus"; 
-  client.channels.cache.get(setting.channelID).join().then(async connection => { 
-    if(client.channels.cache.get(setting.channelID).members.some(member => member.roles.cache.has(setting.modID)) == false) { 
-      connection.play(url, {type: streamType}).on("finish", () => { 
-        play(url); 
-      }); 
-    } else play(url); 
+function play(){ 
+  const url = await ytdlDiscord(videoURL, {
+     filter: "audioonly",
+     opusEncoded: true,
+     encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
+  });
+
+  let type = videoURL.includes("youtube.com") ? "opus" : "ogg/opus"; 
+  client.channels.cache.get(channelID).join().then(connection => { 
+    let isMod = client.channels.cache.get(channelID).members.some(member => member.roles.cache.has(modID));
+    if(!isMod) { 
+      connection.play(url, { type }).on("finish", () => play(url)); 
+    } else {
+      play(url); 
+    }
   }); 
 }  
-client.login("")  
+
+client.login(clientToken)  
